1d4534db13f6461c1bca32e0896fa8cc34e7bd52
K1KC <claresco.rivaine25@gmail.com>
2024-11-07 21:44:56 +0700

    modified:   resources/views/layout/master.blade.php

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,7 +1,7 @@
 @include('import.bootstrap-link')
 <div class="d-flex flex-column min-vh-100" style="background-image: url('{{ asset('images/bg.jpg') }}'); background-size: cover">
 @include('layout.navbar')
-<main class="flex-grow-1 overflow-x-hidden">
+<main class="flex-grow-1 overflow-x-auto">
    @yield('content') 
 </main>
 @include('layout.footer')
